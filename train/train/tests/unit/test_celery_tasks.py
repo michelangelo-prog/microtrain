f006f1d414072cacdf2fd7ae13ae2fcf7314f60d
michalolejarczyk <michal.olejarczyk@yahoo.pl>
2020-02-23 13:56:16 +0100

    add celery task test

--- train/train/tests/unit/test_celery_tasks.py
+++ train/train/tests/unit/test_celery_tasks.py
@@ -2,7 +2,7 @@ import unittest
 from unittest import TestCase
 from unittest.mock import Mock, call, patch
 
-from train.app.beat import celery_headquarter, train_speed
+from train.app.beat import celery_headquarter, train_speed, train_station
 
 
 class TestCeleryPeriodicTasks(TestCase):
@@ -10,6 +10,7 @@ class TestCeleryPeriodicTasks(TestCase):
         # arrange
         celery_headquarter.send_task = Mock()
         self.expected_speed = 99.05
+
         # act
         with patch("train.app.beat.get_train_speed", return_value=self.expected_speed):
             train_speed()
@@ -27,6 +28,33 @@ class TestCeleryPeriodicTasks(TestCase):
         self.assertEqual(2, len(call_args))
         expected = call(
             "periodic.train_speed", ({"actual_speed": self.expected_speed},)
+        )
+        self.assertEqual(expected, call_args)
+
+    def test_train_station(self):
+        # arrange
+        celery_headquarter.send_task = Mock()
+        self.expected_station = "Test"
+
+        # act
+        with patch(
+            "train.app.beat.get_train_station", return_value=self.expected_station
+        ):
+            train_station()
+
+        # assert
+        celery_headquarter.send_task.assert_called_once()
+
+        self.__check_if_proper_task_with_train_station_has_been_send_to_celery_headquarter(
+            celery_headquarter.send_task.call_args_list[0]
+        )
+
+    def __check_if_proper_task_with_train_station_has_been_send_to_celery_headquarter(
+        self, call_args
+    ):
+        self.assertEqual(2, len(call_args))
+        expected = call(
+            "periodic.train_station", ({"train_station": self.expected_station},)
         )
         self.assertEqual(expected, call_args)
 
