cbb6638a464d5780c262fa19e1bc2a2bb34bc2f1
michalolejarczyk <michal.olejarczyk@yahoo.pl>
2020-02-23 09:32:18 +0100

    edit celery tasks

--- train/train/app/beat/__init__.py
+++ train/train/app/beat/__init__.py
@@ -1,13 +1,11 @@
+from datetime import timedelta
+
 from celery import Celery
+from celery.decorators import periodic_task
 
 from train.app.rest import app
 from train.domain.config import CeleryConfig, CeleryHeadquarterConfig
-
 from train.domain.utils import get_train_speed, get_train_station
-
-
-from celery.decorators import periodic_task
-from datetime import timedelta
 
 
 def make_celery(app):
@@ -18,6 +16,7 @@ def make_celery(app):
     )
 
     return celery
+
 
 def make_celery_headquarter(app):
     celery = Celery(
@@ -36,16 +35,12 @@ celery_headquarter = make_celery_headquarter(app)
 @periodic_task(run_every=timedelta(seconds=10))
 def train_speed():
     celery_headquarter.send_task(
-        "periodic.train_speed",
-        (dict(actual_speed=get_train_speed()),)
+        "periodic.train_speed", (dict(actual_speed=get_train_speed()),)
     )
+
 
 @periodic_task(run_every=timedelta(seconds=30))
 def train_station():
     celery_headquarter.send_task(
-        "periodic.train_station",
-        (dict(train_station=get_train_station()),)
+        "periodic.train_station", (dict(train_station=get_train_station()),)
     )
-
-
-
--- train/train/domain/utils.py
+++ train/train/domain/utils.py
@@ -1,23 +1,37 @@
 import random
-import time
-
-random.seed(time.clock())
 
 MINIMAL_SPEED = 0
 MAX_SPEED = 180
 DECIMAL_PLACES = 2
 
 STATIONS = (
+    "Wałcz",
+    "Ostrowiec Wałecki",
+    "Wiesiołka",
+    "Zabrodzie",
+    "Płytnica",
+    "Łowisko",
+    "Tarnówka",
+    "Węgierce",
+    "Annopole",
+    "Klukowo",
+    "Złotów",
     "Mogilno",
     "Sucharzewo",
     "Mokre",
+    "Sławno",
+    "Radosław Sławieński",
+    "Staniewice",
+    "Postomino",
+    "Złakowo",
+    "Duninowo",
 )
 
 
 def get_train_speed():
-
     MULTI = 10 * DECIMAL_PLACES
     return random.randint(MINIMAL_SPEED, MAX_SPEED * MULTI) / MULTI
+
 
 def get_train_station():
     return random.choice(STATIONS)
