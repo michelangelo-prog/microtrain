a82ebc8ae920a5e47c5eeb0f02c2ef9acc5ef959
michalolejarczyk <michal.olejarczyk@yahoo.pl>
2020-02-23 13:27:13 +0100

    add celery task test

--- train/train/tests/unit/test_celery_tasks.py
+++ train/train/tests/unit/test_celery_tasks.py
@@ -1,4 +1,35 @@
 import unittest
+from unittest import TestCase
+from unittest.mock import Mock, call, patch
+
+from train.app.beat import celery_headquarter, train_speed
+
+
+class TestCeleryPeriodicTasks(TestCase):
+    def test_train_speed(self):
+        # arrange
+        celery_headquarter.send_task = Mock()
+        self.expected_speed = 99.05
+        # act
+        with patch("train.app.beat.get_train_speed", return_value=self.expected_speed):
+            train_speed()
+
+        # assert
+        celery_headquarter.send_task.assert_called_once()
+
+        self.__check_if_proper_task_with_train_speed_has_been_send_to_celery_headquarter(
+            celery_headquarter.send_task.call_args_list[0]
+        )
+
+    def __check_if_proper_task_with_train_speed_has_been_send_to_celery_headquarter(
+        self, call_args
+    ):
+        self.assertEqual(2, len(call_args))
+        expected = call(
+            "periodic.train_speed", ({"actual_speed": self.expected_speed},)
+        )
+        self.assertEqual(expected, call_args)
+
 
 if __name__ == "__main__":
     unittest.main()
